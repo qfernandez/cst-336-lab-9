819e07c08e44d59a1aa58ed3db5835fbdfab3536
EC2 Default User <ec2-user@ip-172-31-20-200.us-west-2.compute.internal>
2020-04-20 02:12:19 +0000

    First Commit

---
+++ index.js
@@ -0,0 +1,63 @@
+/* Require external APIs and start our application instance */
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
+
+/* Configure our server to read public folder and ejs files */
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+/* Configure MySQL DBMS */
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: ' ',
+    database: 'quotes_db'
+});
+connection.connect();
+
+/* The handler for the DEFAULT route */
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+/* The handler for the /author route */
+app.get('/author', function(req, res){
+    var stmt = 'select * from l9_author where firstName=\'' 
+                + req.query.firstname + '\' and lastName=\'' 
+                + req.query.lastname + '\';'
+	connection.query(stmt, function(error, found){
+	    var author = null;
+	    if(error) throw error;
+	    if(found.length){
+	        author = found[0];
+	        // Convert the Date type into the String type
+	        author.dob = author.dob.toString().split(' ').slice(0,4).join(' ');
+	        author.dod = author.dod.toString().split(' ').slice(0,4).join(' ');
+	    }
+	    res.render('author', {author: author});
+	});
+});
+
+/* The handler for the /author/name/id route */
+app.get('/author/:aid', function(req, res){
+    var stmt = 'select quote, firstName, lastName ' +
+               'from l9_quotes, l9_author ' +
+               'where l9_quotes.authorId=l9_author.authorId ' + 
+               'and l9_quotes.authorId=' + req.params.aid + ';'
+    connection.query(stmt, function(error, results){
+        if(error) throw error;
+        var name = results[0].firstName + ' ' + results[0].lastName;
+        res.render('quotes', {name: name, quotes: results});      
+    });
+});
+
+/* The handler for undefined routes */
+app.get('*', function(req, res){
+   res.render('error'); 
+});
+
+/* Start the application server */
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Server has been started');
+})
